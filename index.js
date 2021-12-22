9af6c541acf12d85a03beff74ccb134698e04f31
Jonathan Esguerra <EsguerraDesign@gmail.com>
2021-12-23 03:38:42 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     render() {
         return (
             <div id="header">
-                <h1>Hello World</h1>
+                <h1>Hello World!</h1>
             </div>
         );
     }
